2f6aabb483eb6bc2f8e32c4a9a8ad4de08308b43
brnjk22 <brunonicolasocampoperez@gmail.com>
2025-03-17 19:04:39 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 
 # Definir colores
 ROJO='\033[0;31m'
@@ -7,28 +6,28 @@ AMARILLO='\033[1;33m'
 BLANCO='\033[0m'
 
 # Actualizar los repositorios
-echo -e "${AMARILLO}Actualizando los repositorios...${BLANCO}"
+echo -e "${AMARILLO}Actualizando los repositorios${BLANCO}"
 sudo apt update -y
 
 # Instalar Snap
-echo -e "${AMARILLO}Instalando Snap...${BLANCO}"
+echo -e "${AMARILLO}Instalando Snap${BLANCO}"
 sudo apt install snapd -y
 
 # Instalar OpenSSH Server
-echo -e "${AMARILLO}Instalando OpenSSH Server...${BLANCO}"
+echo -e "${AMARILLO}Instalando OpenSSH Server${BLANCO}"
 sudo apt install openssh-server -y
 
 # Instalar dependencias
-echo -e "${AMARILLO}Instalando dependencias...${BLANCO}"
+echo -e "${AMARILLO}Instalando dependencias${BLANCO}"
 sudo apt install wget curl gnupg2 build-essential libpq-dev libreadline-dev libssl-dev libpcap-dev libsqlite3-dev zlib1g-dev -y
 
 # Instalar Nmap
-echo -e "${AMARILLO}Instalando Nmap...${BLANCO}"
+echo -e "${AMARILLO}Instalando Nmap${BLANCO}"
 sudo apt install nmap -y
 
 # Instalar Metasploit Framework
-echo -e "${AMARILLO}Instalando Metasploit Framework...${BLANCO}"
+echo -e "${AMARILLO}Instalando Metasploit Framework${BLANCO}"
 sudo snap install metasploit-framework
 
 # Fin del script
-echo -e "${VERDE}Instalación completada con éxito.${BLANCO}"
+echo -e "${VERDE}Instalación finalizada${BLANCO}"
